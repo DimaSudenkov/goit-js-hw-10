7e722be74175493d2057c96e32e3d007fdc8268c
DimaSudenkov <sudenkovdimas@gmail.com>
2023-10-24 14:47:26 +0300

    full task

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import { fetchBreeds } from "./cat-api";
 import { fetchCatByBreed } from "./cat-api";
 
+import SlimSelect from 'slim-select';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import './style.css';
+import 'slim-select/dist/slimselect.css';
 
 const selectors = {
   selectBreed: document.querySelector('.breed-select'),
   loaderText: document.querySelector('.loader'),
   errorText: document.querySelector('.error'),
-  catInfo: document.querySelector('.cat-info')
+  catInfo: document.querySelector('.cat-info'),
 }
 
-selectors.selectBreed.style.visibility = 'hidden';
-selectors.loaderText.style.visibility = 'visible';
-selectors.errorText.style.visibility = 'hidden';
+selectors.selectBreed.style.display = 'none';
+selectors.loaderText.style.display = 'block';
+selectors.errorText.style.display = 'none';
 
+let arrBreedsId = [];
 fetchBreeds()
   .then(data => {
     data.forEach(breed => {
@@ -20,22 +26,33 @@ fetchBreeds()
       option.value = breed.id;
       option.text = breed.name;
       selectors.selectBreed.appendChild(option);
+      arrBreedsId.push({ text: breed.name, value: breed.id });
     });
   }).then(() => {
-    selectors.selectBreed.style.visibility = 'visible';
-    selectors.loaderText.style.visibility = 'hidden';
+    selectors.selectBreed.style.display = 'block';
+    selectors.loaderText.style.display = 'none';
+  }).then(() => {
+    new SlimSelect({
+      select: '.breed-select',
+      data: arrBreedsId,
+    })
   })
   .catch(() => {
-    selectors.selectBreed.style.visibility = 'hidden';
-    selectors.loaderText.style.visibility = 'hidden';
-    selectors.errorText.style.visibility = 'visible';
+    Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+      width: '400px',
+      position: 'center-center',
+      timeout: '5000'
+    })
+    selectors.selectBreed.style.display = 'none';
+    selectors.loaderText.style.display = 'none';
+    selectors.errorText.style.display = 'block';
   })
 
 selectors.selectBreed.addEventListener('change', handlerChange)
 
 function handlerChange(evt) {
   const breedId = evt.target.value;
-  selectors.loaderText.style.visibility = 'visible';
+  selectors.loaderText.style.display = 'block';
   fetchCatByBreed(breedId)
     .then((data) => selectors.catInfo.innerHTML = markupBreed(data))
     .then(() => selectors.loaderText.style.visibility = 'hidden')
@@ -44,11 +61,10 @@ function handlerChange(evt) {
 
 function markupBreed(arr) {
   return arr.map(({ url, breeds: { [0]: { name, description, temperament } } }) =>
-    `<img src="${url}" alt="${name}" width = '300'/>
+    `<img src="${url}" alt="${name}" width = '350'/>
         <h2>${name}</h2>
       <p>${description}</p>
-      <p><b>Temperament:</b>${temperament}</p>
-      `
+      <p><b>Temperament:</b>${temperament}</p>`
   ).join('')
 }
 
